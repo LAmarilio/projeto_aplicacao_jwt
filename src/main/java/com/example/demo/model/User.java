9080b47ee9bea9deb5af2f830f48b4e8afad19c7
Leonardo Amarilio <leoamarilio1606@gmail.com>
2026-01-28 14:13:31 -0300

    Create enum Role for Users.
    Create attributes from model User.

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,39 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "full_name", nullable = false)
+    @NotBlank
+    private String fullName;
+
+    @Column(name = "email", unique = true)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 8)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
